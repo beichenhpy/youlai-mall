f2ea20be0ddb3e94c19b6fd22f8e99828afb2220
beichenhpy <beichenhpy@outlook.com>
2021-06-25 12:08:49 +0800

    fix(gateway:utils/ResponseUtils):代码优化

--- youlai-gateway/src/main/java/com/youlai/gateway/util/ResponseUtils.java
+++ youlai-gateway/src/main/java/com/youlai/gateway/util/ResponseUtils.java
@@ -12,6 +12,7 @@ import org.springframework.http.server.reactive.ServerHttpResponse;
 import reactor.core.publisher.Mono;
 
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 /**
  * @Author hxr
@@ -19,13 +20,13 @@ import java.nio.charset.Charset;
  */
 public class ResponseUtils {
 
-    public static Mono writeErrorInfo(ServerHttpResponse response, ResultCode resultCode){
+    public static Mono<Void> writeErrorInfo(ServerHttpResponse response, ResultCode resultCode){
         response.setStatusCode(HttpStatus.OK);
         response.getHeaders().set(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
         response.getHeaders().set("Access-Control-Allow-Origin", "*");
         response.getHeaders().set("Cache-Control", "no-cache");
         String body = JSONUtil.toJsonStr(Result.failed(resultCode));
-        DataBuffer buffer = response.bufferFactory().wrap(body.getBytes(Charset.forName("UTF-8")));
+        DataBuffer buffer = response.bufferFactory().wrap(body.getBytes(StandardCharsets.UTF_8));
         return response.writeWith(Mono.just(buffer))
                 .doOnError(error -> DataBufferUtils.release(buffer));
     }
